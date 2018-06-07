f7b3ee87b49f9bea3af083887240e535c127aacb
Chen <lihui.chen@accenture.com>
2018-06-07 17:40:13 +0800

    add detection onpush

--- web-app/src/app/modules/module-one/module-one-component/module-one-component.component.ts
+++ web-app/src/app/modules/module-one/module-one-component/module-one-component.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, ViewChild, ElementRef, ViewContainerRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, ViewContainerRef, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-module-one-component',
   templateUrl: './module-one-component.component.html',
-  styleUrls: ['./module-one-component.component.css']
+  styleUrls: ['./module-one-component.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ModuleOneComponentComponent implements OnInit {
   @ViewChild('inputOne') inputOne: ElementRef;
@@ -12,12 +13,17 @@ export class ModuleOneComponentComponent implements OnInit {
   jouneryy: string = "test";
   inputOneModel: string = "one";
   inputTwoModel: string = "two";
+  test: number = 0;
 
 
-  constructor() { }
+  constructor(private ref: ChangeDetectorRef) { }
 
   ngOnInit() {
     console.log(this.inputOne, this.inputTwo);
+    setInterval(() => {
+      this.test ++;
+      this.ref.markForCheck();
+    }, 1000);
   }
 
 }
